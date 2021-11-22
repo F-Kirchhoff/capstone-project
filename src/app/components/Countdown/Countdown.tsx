c4a839695768a8c29c2f5cf05c78553a917d5916
Felix Kirchhoff <mailfk@phispace.eu>
2021-11-22 16:22:56 +0100

    Add: Countdown logic and story

--- src/app/components/Countdown/Countdown.tsx
+++ src/app/components/Countdown/Countdown.tsx
@@ -1,1 +1,27 @@
-rrc
+import React from 'react'
+
+type CountdownProps = {
+  deadline: number
+}
+
+export default function Countdown({ deadline }: CountdownProps): JSX.Element {
+  const diff = deadline - new Date().getTime()
+
+  const isDue = diff < 0
+  const yearDiff = Math.floor(diff / (1000 * 60 * 60 * 24 * 365))
+  const monthDiff = Math.floor(diff / (1000 * 60 * 60 * 24 * 30))
+  const dayDiff = Math.floor(diff / (1000 * 60 * 60 * 24))
+  const hourDiff = Math.floor(diff / (1000 * 60 * 60))
+
+  const countdownString = isDue
+    ? '!'
+    : yearDiff
+    ? `${yearDiff} y`
+    : monthDiff
+    ? `${monthDiff} m`
+    : dayDiff
+    ? `${dayDiff} d`
+    : `${hourDiff} h`
+
+  return <div>{countdownString}</div>
+}
