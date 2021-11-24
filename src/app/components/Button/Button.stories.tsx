62b636472777d386025626b2b2549443924432b4
Felix Kirchhoff <mailfk@phispace.eu>
2021-11-24 16:31:39 +0100

    Add: Story for testing Button onClick functionality

--- src/app/components/Button/Button.stories.tsx
+++ src/app/components/Button/Button.stories.tsx
@@ -10,3 +10,6 @@ export const Regular = (): JSX.Element => <Button>A test button</Button>
 export const Highlight = (): JSX.Element => (
   <Button highlight>Highlight!</Button>
 )
+export const With_Action = (): JSX.Element => (
+  <Button onClick={() => alert('Test Passed!')}>A test button</Button>
+)
