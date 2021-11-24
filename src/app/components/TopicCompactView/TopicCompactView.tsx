f76ba31776b4a54cc2f598666ab9f7f535b2bbdc
Felix Kirchhoff <mailfk@phispace.eu>
2021-11-24 14:21:29 +0100

    Style: add box shadow

--- src/app/components/TopicCompactView/TopicCompactView.tsx
+++ src/app/components/TopicCompactView/TopicCompactView.tsx
@@ -23,4 +23,6 @@ const TopicCompact = styled.li`
   border-radius: 15px;
   font-family: 'Plairfair';
   font-weight: bold;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
 `
