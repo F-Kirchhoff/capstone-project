5747d58255d52c80027f51af41594d54e1195277
Felix Kirchhoff <mailfk@phispace.eu>
2021-12-02 12:24:22 +0100

    Fix: remove console log and add req and res types

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import express from 'express'
+import type { Request, Response } from 'express'
 import { connectToDB } from '../utils/db'
 import api from './routes/api'
 
@@ -16,12 +17,11 @@ app.use('/storybook', express.static('dist/storybook'))
 
 app.use(express.static('dist/app'))
 
-app.get('*', (_request, response) => {
-  response.sendFile('index.html', { root: 'dist/app' })
+app.get('*', (_req: Request, res: Response) => {
+  res.sendFile('index.html', { root: 'dist/app' })
 })
 
 const url = process.env.MONGODB_API_KEY || ''
-console.log(url)
 connectToDB(url).then(() => {
   app.listen(port, () => {
     console.log(`Server listening on port ${port}!`)
