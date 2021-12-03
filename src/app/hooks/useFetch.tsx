299e2ea99c82476ea6b3bd51db1cc077f24e5d6e
Felix Kirchhoff <mailfk@phispace.eu>
2021-12-03 11:11:02 +0100

    Add: switch cases for GET and POSt in useFetch

--- src/app/hooks/useFetch.tsx
+++ src/app/hooks/useFetch.tsx
@@ -1,24 +1,43 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 function useFetch<Type>(
   method: string,
   url: string,
-  body = undefined
+  body?: any
 ): [Type | null, () => void] {
   const [data, setData] = useState(null)
 
   async function fetchData() {
-    const res = await fetch(url, {
-      method,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    if (res.status === 200) {
-      const fetchedData = await res.json()
-      setData(fetchedData)
-    } else {
-      console.error(`${res.status}: Something went wrong :/`)
+    switch (method) {
+      case 'GET': {
+        const res = await fetch(url)
+        if (res.status === 200) {
+          const fetchedData = await res.json()
+          setData(fetchedData)
+        } else {
+          console.error(`${res.status}: Something went wrong :/`)
+        }
+        return
+      }
+
+      case 'POST': {
+        const res = await fetch(url, {
+          method,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body,
+        })
+        if (res.status === 200) {
+          const fetchedData = await res.json()
+          setData(fetchedData)
+        } else {
+          console.error(`${res.status}: Something went wrong :/`)
+        }
+        return
+      }
+      default:
+        console.error(`Error: ${method} is not a valid http type.`)
     }
   }
 
