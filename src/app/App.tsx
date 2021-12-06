425e41a826f3ece6d81dac4abd61a23a6a18cad5
Felix Kirchhoff <mailfk@phispace.eu>
2021-12-06 15:12:38 +0100

    Remove: outdaded imports

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard/Dashboard'
 
