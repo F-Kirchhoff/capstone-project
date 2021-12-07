2db1f6751aa64548351885c8c12940a6e25c8d58
Felix Kirchhoff <mailfk@phispace.eu>
2021-12-07 17:40:04 +0100

    Add: SliderMenu component

---
+++ src/app/components/SliderMenu/SliderMenu.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import styled from 'styled-components'
+
+type SliderMenuProps = {
+  options: {
+    id: string
+    text: string
+  }[]
+  selectedOption: string
+  onSelect: (option: string) => void
+}
+
+export default function SliderMenu({
+  options,
+  selectedOption,
+  onSelect,
+}: SliderMenuProps): JSX.Element {
+  return (
+    <Container>
+      {options.map(option => (
+        <Option
+          active={selectedOption === option.id}
+          onClick={() => onSelect(option.id)}
+        >
+          {option.text}
+        </Option>
+      ))}
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  display: flex;
+`
+
+const Option = styled.button<{ active: boolean }>`
+  flex-grow: 1;
+  text-align: center;
+  padding: 10px 0;
+  cursor: pointer;
+  border: none;
+  background-color: transparent;
+  transition: 0.1s ease;
+  border-bottom: ${({ active }) =>
+    active ? 'solid 3px var(--c-secondary)' : 'solid 3px transparent'};
+`
