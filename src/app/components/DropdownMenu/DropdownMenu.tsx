ef303f5da215cd9b42bd9878a727a7928bca7cf9
Felix Kirchhoff <mailfk@phispace.eu>
2021-12-09 14:56:59 +0100

    Add: trnnsparent option for overlay wrapper

--- src/app/components/DropdownMenu/DropdownMenu.tsx
+++ src/app/components/DropdownMenu/DropdownMenu.tsx
@@ -4,5 +4,16 @@ export default function DropdownMenu({
   children,
 }: React.PropsWithChildren<React.ReactFragment>): JSX.Element {
   const [showMenu, setShowMenu] = useState(false)
-  return <div></div>
+  return (
+    <>
+      // icon
+      {showMenu && (
+        <MenuContainer>
+          {children.map(child => (
+            <MenuItem></MenuItem>
+          ))}
+        </MenuContainer>
+      )}
+    </>
+  )
 }
