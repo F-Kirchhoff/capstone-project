029384c6e57250145bd181ac53af75b58d6b9b88
Felix Kirchhoff <mailfk@phispace.eu>
2021-12-10 14:36:38 +0100

    Remove: obsolete imports

--- src/app/pages/ProposalView/ProposalView.tsx
+++ src/app/pages/ProposalView/ProposalView.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Link, useNavigate, useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import styled from 'styled-components'
 import Button from '../../components/Button/Button'
 import OverlayWrapper from '../../components/OverlayWrapper/OverlayWrapper'
