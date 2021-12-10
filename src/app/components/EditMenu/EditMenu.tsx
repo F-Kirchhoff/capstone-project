04ce59775be02b41b36711f2768397b36adc49ba
Felix Kirchhoff <mailfk@phispace.eu>
2021-12-10 11:47:20 +0100

    Add: reusable EditMenu component

---
+++ src/app/components/EditMenu/EditMenu.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { FaBan, FaEdit } from 'react-icons/fa'
+import DropdownMenu, { MenuItem } from '../DropdownMenu/DropdownMenu'
+
+type EditMenuProps = {
+  onEdit: () => void
+  onDelete: () => void
+}
+
+export default function EditMenu({ onEdit, onDelete }: EditMenuProps) {
+  return (
+    <DropdownMenu>
+      <MenuItem onClick={onEdit}>
+        <FaEdit />
+        <span>edit</span>
+      </MenuItem>
+      <MenuItem onClick={onDelete}>
+        <FaBan color="var(--c-alert)" />
+        <span>delete</span>
+      </MenuItem>
+    </DropdownMenu>
+  )
+}
