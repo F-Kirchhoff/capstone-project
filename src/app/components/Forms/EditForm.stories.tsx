0c105e66ea75794ef88b133b665253c160418b5b
Felix Kirchhoff <mailfk@phispace.eu>
2021-12-13 13:01:34 +0100

    Add: EditForm story

---
+++ src/app/components/Forms/EditForm.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import EditForm from './EditForm'
+
+export default {
+  title: 'Component/EditForm',
+  component: EditForm,
+}
+
+export const Need = (): JSX.Element => (
+  <EditForm
+    onSubmit={console.log}
+    onCancel={() => console.log('Cancel Button Clicked!')}
+    content={{ description: 'I have a dream.' }}
+  />
+)
+export const Topic = (): JSX.Element => (
+  <EditForm
+    onSubmit={console.log}
+    onCancel={() => console.log('Cancel Button Clicked!')}
+    content={{ title: 'Title for my speech', description: 'I have a dream.' }}
+  />
+)
