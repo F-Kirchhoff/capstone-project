b964aefa5eadcdbf4408291c2293eee17d6e427a
Felix Kirchhoff <mailfk@phispace.eu>
2021-12-17 11:20:15 +0100

    Add: POST route for users

--- src/server/routes/users.ts
+++ src/server/routes/users.ts
@@ -3,9 +3,9 @@ import type { Response, Request } from 'express'
 import type { fetchBody } from '../../app/types/types'
 import { getUsers } from '../../utils/db'
 
-const boards = express.Router()
+const users = express.Router()
 
-boards.get('/', async (req: Request, res: Response) => {
+users.get('/', async (req: Request, res: Response) => {
   const { username }: fetchBody = req.query
 
   const users = await getUsers()
@@ -19,4 +19,35 @@ boards.get('/', async (req: Request, res: Response) => {
   res.send(user.public)
 })
 
-export default boards
+users.post('/', async (req: Request, res: Response) => {
+  const {
+    payload: { username, email, password },
+  }: fetchBody = req.query
+
+  const users = await getUsers()
+  const user = await users.findOne({
+    $or: [{ 'private.email': email }, { 'public.username': username }],
+  })
+
+  if (user) {
+    res.status(422).send(`Error: email or username already in use.`)
+    return
+  }
+
+  const newUser = {
+    public: {
+      username,
+      boards: [],
+    },
+    private: {
+      email,
+      password,
+    },
+  }
+
+  users.insertOne(newUser)
+
+  res.send(`Success: User ${username} added`)
+})
+
+export default users
