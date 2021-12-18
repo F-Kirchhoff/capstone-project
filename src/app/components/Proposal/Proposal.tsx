8ad67f3b3987df8a7621e6e55d173e583c07a341
Felix Kirchhoff <mailfk@phispace.eu>
2021-12-18 17:08:25 +0100

    Cleanup: remove outdated imports

--- src/app/components/Proposal/Proposal.tsx
+++ src/app/components/Proposal/Proposal.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import type { Proposal as ProposalType, User } from '../../types/types'
 import {
