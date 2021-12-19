8ae9fd24af8ede6825b5f2645639f7d375ac6b41
Felix Kirchhoff <mailfk@phispace.eu>
2021-12-19 09:58:16 +0100

    Add: EditBoard page

---
+++ src/app/pages/EditBoard/EditBoard.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router'
+import { useParams } from 'react-router-dom'
+import BoardForm from '../../components/Forms/BoardForm'
+
+import useFetch from '../../hooks/useFetch'
+
+import type { Board } from '../../types/types'
+
+export default function EditBoard(): JSX.Element {
+  const nav = useNavigate()
+
+  const { boardName } = useParams()
+
+  const [board, fetchBoard] = useFetch<Board>(`/api/boards`, { boardName })
+
+  useEffect(() => {
+    fetchBoard('GET')
+  }, [])
+
+  async function handleSubmit(name: string, users: string[]) {
+    const payload = {
+      name,
+      users,
+    }
+
+    await fetchBoard('PATCH', { payload })
+    nav(-1)
+  }
+
+  function handleCancel() {
+    nav(-1)
+  }
+
+  if (!board) {
+    return <></>
+  }
+
+  return (
+    <BoardForm
+      board={{ name: board.name, users: board.users }}
+      onSubmit={handleSubmit}
+      onCancel={handleCancel}
+    />
+  )
+}
