bd330b0dfbaf4f117b26b02b1288538f78a58742
Felix Kirchhoff <mailfk@phispace.eu>
2021-12-20 13:05:32 +0100

    Style: improve upvote styling

--- src/app/GlobalStyle.tsx
+++ src/app/GlobalStyle.tsx
@@ -29,7 +29,10 @@ const GlobalStyle = createGlobalStyle`
     --c-alert: #e41a4c;
     --c-success: #2da86b;
     --c-warning: #cf9924;
-    --c-gradient-highlight: linear-gradient(230deg, #eca945 0%, #e041a1 33%, #39267e 100%);
+    --g-hl-start:#eca945;
+    --g-hl-mid:#e041a1;
+    --g-hl-end:#39267e;
+    --c-gradient-highlight: linear-gradient(230deg, var(--g-hl-start) 0%, var(--g-hl-mid)  33%, var(--g-hl-end)  100%);
     --c-gradient-primary: linear-gradient(230deg,#6d0e55 0%, var(--c-primary), #0f0c22 100%);
   }
 
